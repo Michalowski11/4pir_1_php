6462b2c68a20756c65e56d9f722a200d50d99624
Michalowski11 <michrych007@gmail.com>
2022-11-15 15:47:16 +0100

    drugi commit

--- proba.php
+++ proba.php
@@ -1,3 +1,3 @@
 <?php
-    echo "<h1>Witaj na mojej stronie<h1><br><h2>Autor: Michał Wołoch 4pir_1 :)</h2>"
+    echo "<h1>Witaj na mojej stronie<h1><br><h2>Autor: Michał Wołoch 4pir_1</h2>"
     ?>
